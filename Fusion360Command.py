20da001b80e8c064ade966e0a687310ac2aab305
Patrick Rainsberry <prrainsberry@yahoo.com>
2017-01-12 23:02:20 -0800

    Removed unnecessary comments

--- Fusion360Command.py
+++ Fusion360Command.py
@@ -2,26 +2,20 @@ import adsk.core, adsk.fusion, traceback
 
 from .Fusion360CommandBase import Fusion360CommandBase
 
-# The following will define a command in a tool bar panel
 class Fusion360Command(Fusion360CommandBase):
     
-    # Runs when Fusion command would generate a preview after all inputs are valid or changed
     def on_preview(self, command, command_inputs, args, input_values):
         pass
     
-    # Runs when the command is destroyed.  Sometimes useful for cleanup after the fact
     def on_destroy(self, command, command_inputs, reason, input_values):    
         pass
     
-    # Runs when when any input in the command dialog is changed
     def on_inputChanged(self, command, command_inputs, changed_input, input_values):
         pass
     
-    # Runs when the user presses ok button
     def on_execute(self, command, command_inputs, args, input_values):
         pass
     
-    # Runs when user selects your command from Fusion UI, Build UI here
     def on_create(self, command, command_inputs):
         
         # Create a few inputs in the UI
